a32cf404bff4ef6d645bab55f53f213a57de05f5
sk8antoniosk8 <81818710+sk8antoniosk8@users.noreply.github.com>
2022-05-20 19:23:40 +0200

    Delete scripts directory

--- scripts/menuresponsive_buttons.js
+++
@@ -1,5 +0,0 @@
-
-//Efecto de slide (de arriba a abajo y viceversa) al hacer click en el elemento con la clase indicada.
-$("#buttonburguermenu").click(function(){
-  $(".header-menu-responsive").slideToggle(200).addClass('.mostrar');
-});
